6d0fc5c867e882fe262a1e46b69f51d505ac5f54
thewoodfish <jasonholt2002@gmail.com>
2024-05-20 01:30:00 +0100

    add: repo initialized

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+/// Copyright (c) 2024  Algorealm, Inc 
+/// The samaritan network
+
+
+/// Here's the main entry of network
+/// We read commands from the terminal and handle them internally, printing results to stdout
+#[tokio::main]
+async fn main()  {
+    
+}
+
+
+// save <app_did> <owner_did> <key> <value>
+// get <app_did> <owner_did> <key>
+
+
